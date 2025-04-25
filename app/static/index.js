375cd9568c069406f6b41c458365ace8fd6ff8bf
yliu0585 <yliu0585@student.monash.edu>
2025-04-25 22:41:44 +1000

    update route

--- app/static/index.js
+++ app/static/index.js
@@ -3,14 +3,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const btn1 = document.getElementById('epic1');
     const btn2 = document.getElementById('epic2');
+    const btn3 = document.getElementById('epic3');
   
-    if (btn1 && btn2) {
+    if (btn1 && btn2 &&btn3) {
       btn1.addEventListener('click', function () {
         window.location.href = '/epic1';
       });
   
       btn2.addEventListener('click', function () {
         window.location.href = '/epic2';
+      });
+      btn3.addEventListener('click', function () {
+        window.location.href = '/story';
       });
     }
   });
